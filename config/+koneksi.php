37dbc241270b81acfc9449d70d5c68ff445f1335
ekkyardian <ekkyardian@outlook.com>
2020-05-17 22:21:16 +0700

    change database to heroku

--- config/+koneksi.php
+++ config/+koneksi.php
@@ -6,8 +6,8 @@
  * Time: 20:18
  */
 
-$host       = "localhost";
-$user       = "root";
-$pass       = "";
-$database   = "db_spbl";
+$host       = "ec2-52-202-146-43.compute-1.amazonaws.com";
+$user       = "ufsjqqqpokpgdh";
+$pass       = "e7820bfcbba8f00c4b2bffc10f6a79540f4bdede391b9cd40eaaa8589e8eab71";
+$database   = "dc1b0t4rha12nb";
 ?>
