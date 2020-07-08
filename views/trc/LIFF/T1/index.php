e220c4884d0f830e6d4aad6db2d8c860b5b0a47c
ekkyardian <ekkyardian@outlook.com>
2020-07-08 19:53:48 +0700

    Menghapus JavaScript LIFF yang tidak dibutuhkan

--- views/trc/LIFF/T1/index.php
+++ views/trc/LIFF/T1/index.php
@@ -293,76 +293,45 @@ $LiffLaporanObservasi = new LiffLaporanObservasi($connection)
                                 }
                                 ?>
                             </form>
+
+                            <div id="statusMessage">
+                                <div id="isInClientMessage"></div>
+                                <div id="apiReferenceMessage">
+                                    <p>Available LIFF methods vary depending on the browser you use to open the LIFF app.</p>
+                                    <p>Please refer to the <a href="https://developers.line.biz/en/reference/liff/#initialize-liff-app">API reference page</a> for more information.</p>
+                                </div>
+                            </div>
+
+                            <!-- LIFF ID ERROR -->
+                            <div id="liffIdErrorMessage" class="hidden">
+                                <p>You have not assigned any value for LIFF ID.</p>
+                                <p>If you are running the app using Node.js, please set the LIFF ID as an environment variable in your Heroku account follwing the below steps: </p>
+                                <code id="code-block">
+                                    <ol>
+                                        <li>Go to `Dashboard` in your Heroku account.</li>
+                                        <li>Click on the app you just created.</li>
+                                        <li>Click on `Settings` and toggle `Reveal Config Vars`.</li>
+                                        <li>Set `MY_LIFF_ID` as the key and the LIFF ID as the value.</li>
+                                        <li>Your app should be up and running. Enter the URL of your app in a web browser.</li>
+                                    </ol>
+                                </code>
+                                <p>If you are using any other platform, please add your LIFF ID in the <code>index.html</code> file.</p>
+                                <p>For more information about how to add your LIFF ID, see <a href="https://developers.line.biz/en/reference/liff/#initialize-liff-app">Initializing the LIFF app</a>.</p>
+                            </div>
+
+                            <!-- LIFF INIT ERROR -->
+                            <div id="liffInitErrorMessage" class="hidden">
+                                <p>Something went wrong with LIFF initialization.</p>
+                                <p>LIFF initialization can fail if a user clicks "Cancel" on the "Grant permission" screen, or if an error occurs in the process of <code>liff.init()</code>.</p>
+                            </div>
+
+                            <!-- NODE.JS LIFF ID ERROR -->
+                            <div id="nodeLiffIdErrorMessage" class="hidden">
+                                <p>Unable to receive the LIFF ID as an environment variable.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <!-- LIFF DATA -->
-<!--                <div id="liffData">-->
-<!--                    <h2 id="liffDataHeader" class="textLeft">LIFF Data</h2>-->
-<!--                    <table>-->
-<!--                        <tr>-->
-<!--                            <th>OS</th>-->
-<!--                            <td id="deviceOS" class="textLeft"></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <th>Language</th>-->
-<!--                            <td id="browserLanguage" class="textLeft"></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <th>LIFF SDK Version</th>-->
-<!--                            <td id="sdkVersion" class="textLeft"></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <th>LINE Version</th>-->
-<!--                            <td id="lineVersion" class="textLeft"></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <th>isInClient</th>-->
-<!--                            <td id="isInClient" class="textLeft"></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <th>isLoggedIn</th>-->
-<!--                            <td id="isLoggedIn" class="textLeft"></td>-->
-<!--                        </tr>-->
-<!--                    </table>-->
-<!--                </div>-->
-
-                <div id="statusMessage">
-                    <div id="isInClientMessage"></div>
-                    <div id="apiReferenceMessage">
-                        <p>Available LIFF methods vary depending on the browser you use to open the LIFF app.</p>
-                        <p>Please refer to the <a href="https://developers.line.biz/en/reference/liff/#initialize-liff-app">API reference page</a> for more information.</p>
-                    </div>
-                </div>
-            </div>
-
-            <!-- LIFF ID ERROR -->
-            <div id="liffIdErrorMessage" class="hidden">
-                <p>You have not assigned any value for LIFF ID.</p>
-                <p>If you are running the app using Node.js, please set the LIFF ID as an environment variable in your Heroku account follwing the below steps: </p>
-                <code id="code-block">
-                    <ol>
-                        <li>Go to `Dashboard` in your Heroku account.</li>
-                        <li>Click on the app you just created.</li>
-                        <li>Click on `Settings` and toggle `Reveal Config Vars`.</li>
-                        <li>Set `MY_LIFF_ID` as the key and the LIFF ID as the value.</li>
-                        <li>Your app should be up and running. Enter the URL of your app in a web browser.</li>
-                    </ol>
-                </code>
-                <p>If you are using any other platform, please add your LIFF ID in the <code>index.html</code> file.</p>
-                <p>For more information about how to add your LIFF ID, see <a href="https://developers.line.biz/en/reference/liff/#initialize-liff-app">Initializing the LIFF app</a>.</p>
-            </div>
-
-            <!-- LIFF INIT ERROR -->
-            <div id="liffInitErrorMessage" class="hidden">
-                <p>Something went wrong with LIFF initialization.</p>
-                <p>LIFF initialization can fail if a user clicks "Cancel" on the "Grant permission" screen, or if an error occurs in the process of <code>liff.init()</code>.</p>
-            </div>
-
-            <!-- NODE.JS LIFF ID ERROR -->
-            <div id="nodeLiffIdErrorMessage" class="hidden">
-                <p>Unable to receive the LIFF ID as an environment variable.</p>
             </div>
 
             <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
