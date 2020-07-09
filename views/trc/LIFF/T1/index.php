8ecce286b4a001287931d358e9d1e6a6b3fd693d
ekkyardian <ekkyardian@outlook.com>
2020-07-09 19:02:14 +0700

    Try: Pass JavaScript variable to PHP via AJAX (P:13)

--- views/trc/LIFF/T1/index.php
+++ views/trc/LIFF/T1/index.php
@@ -140,7 +140,7 @@ $LiffLaporanObservasi = new LiffLaporanObservasi($connection);
                             type: "POST",
                             data: {lineID: lineID },
                             success: function(data) {
-                                $('#result').html(data);
+                                $('#result').text(data);
                             }
                         });
                     });
