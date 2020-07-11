61c8139474dbffd2e373c27113f4bf274a72be3c
ekkyardian <ekkyardian@outlook.com>
2020-07-11 16:44:53 +0700

    Try: get data and permission for input hasil observasi tahap 1 (P: 13)

--- views/trc/LIFF/T1/index.php
+++ views/trc/LIFF/T1/index.php
@@ -13,17 +13,38 @@ require_once ('../../../../models/trc/liff/LiffLaporanObservasi.php');
 $connection = new Database($host, $user, $pass, $database);
 $LiffLaporanObservasi = new LiffLaporanObservasi($connection);
 
-if (isset($_POST['ajax']) && isset($_POST['lineID']) ){
+if( isset($_POST['ajax']) && isset($_POST['lineID']) ){
     $lineID = $_POST['lineID'];
 
-    if ($lineID == "Ua275161a7af915419f9dd93c19904bdc") {
-        include "input_laporan_t1.php";
+    // Mengambil ID USER berdasarkan ID LINE yang digunakan untuk login LIFF
+    $ambilIdUser = $LiffLaporanObservasi->cek_user($lineID);
+    $dataIdUser = $ambilIdUser->fetchObject();
+    $hasil_idUser = $dataIdUser->id_user;
+
+    // Mengecek apakah ada penugasan untuk User dengan ID LINE tersebut
+    $peristiwa = $LiffLaporanObservasi->cek_peristiwa($hasil_idUser);
+    $cekPeristiwa = $peristiwa->rowCount();
+
+    if ($cekPeristiwa > 0) {
+        $tampil_peristiwa = $peristiwa->fetchObject();
+        $hasil_peristiwa = $tampil_peristiwa->id_peristiwa;
+        $tampil_observasi = $LiffLaporanObservasi->tampil_observasi1($hasil_peristiwa)->fetchObject();
+
+        // Pengecekan
+        echo "Id User: ".$hasil_idUser;
+        echo "<br />";
+        echo "Id LINE: ".$lineID;
+        echo "<br />";
+        echo "Id Peristiwa: ".$hasil_peristiwa;
     }
     else {
-        include "informasi.php";
+        echo "
+              <script>
+                  document.getElementById(\"liffAppContent\").classList.add('hidden');
+                  document.getElementById(\"tidakAdaPenugasan\").classList.remove('hidden');
+              </script>
+        ";
     }
-
-    exit;
 }
 
 //require_once ('../../../../config/+koneksi.php');
@@ -104,6 +125,10 @@ if (isset($_POST['ajax']) && isset($_POST['lineID']) ){
                 </div>
             </div>
 
+            <?php
+            exit;
+            ?>
+
             <form method='post' action>
                 <p id='response'></p>
             </form>
@@ -155,6 +180,125 @@ if (isset($_POST['ajax']) && isset($_POST['lineID']) ){
                                 <button id="liffLoginButton">Log in</button>
                                 <button id="liffLogoutButton">Log out</button>
                             </div>
+
+                            <div class="space-10"></div>
+
+                            <!-- NOTIFIKASI PROSES INPUT LAPORAN OBSERVASI -->
+                            <?php
+                            if(isset($_GET['action'])) {
+                                if($_GET['action']=='failed') {
+                                    echo "<div class='alert alert-danger'>
+                                          <button type='button' class='close' data-dismiss='alert'>
+                                              <i class='ace-icon fa fa-times'></i>
+                                          </button> Gagal mengirim laporan
+                                          </div>
+                                    ";
+                                } elseif ($_GET['action']=='success') {
+                                    echo "<div class='alert alert-success'>
+                                         <button type='button' class='close' data-dismiss='alert'>
+                                             <i class='ace-icon fa fa-times'></i>
+                                         </button> <i class='ace-icon fa fa-check'></i> Laporan terkirim
+                                          </div>
+                                    ";
+                                }
+                            }
+                            ?>
+
+                            <!-- HALAMAN INPUT LAPORAN OBSERVASI -->
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <label for="korban_terdampak">
+                                    Korban Terdampak
+                                </label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control input-mask-product col-xs-11 col-sm-11 col-lg-11"
+                                           id="korban_terdampak" name="korban_terdampak" value="<?php echo $tampil_observasi->korban_terdampak; ?>" />
+                                    <span class="input-group-addon">
+                                        Orang
+                                    </span>
+                                </div>
+
+                                <label for="korban_mengungsi">
+                                    Korban Mengungsi
+                                </label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control input-mask-product col-xs-11 col-sm-11 col-lg-11"
+                                           id="korban_mengungsi" name="korban_mengungsi" value="<?php echo $tampil_observasi->korban_mengungsi; ?>" />
+                                    <span class="input-group-addon">Orang</span>
+                                </div>
+
+                                <label for="korban_luka">
+                                    Korban Luka
+                                </label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control input-mask-product col-xs-11 col-sm-11 col-lg-11"
+                                           id="korban_luka" name="korban_luka" value="<?php echo $tampil_observasi->korban_luka; ?>" />
+                                    <span class="input-group-addon">Orang</span>
+                                </div>
+
+                                <label for="korban_meninggal">
+                                    Korban Meninggal
+                                </label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control input-mask-product col-xs-11 col-sm-11 col-lg-11"
+                                           id="korban_meninggal" name="korban_meninggal"  value="<?php echo $tampil_observasi->korban_meninggal; ?>" />
+                                    <span class="input-group-addon">Orang</span>
+                                </div>
+
+                                <label for="korban_hilang">
+                                    Korban Hilang
+                                </label>
+                                <div class="input-group">
+                                    <input type="number" class="form-control input-mask-product col-xs-11 col-sm-11 col-lg-11"
+                                           id="korban_hilang" name="korban_hilang" value="<?php echo $tampil_observasi->korban_hilang; ?>" />
+                                    <span class="input-group-addon">Orang</span>
+                                </div>
+
+                                <label for="pasca_bencana">
+                                    Kondisi Pasca Bencana
+                                </label>
+                                <div class="input-group">
+                                    <select class="form-control col-xs-12 col-sm-12 col-lg-12" name="pasca_bencana" id="pasca_bencana">
+                                        <option value="<?php echo $tampil_observasi->pasca_bencana; ?>"><?php echo $tampil_observasi->pasca_bencana; ?></option>
+                                        <option value="Normal">Normal</option>
+                                        <option value="Waspada">Waspada</option>
+                                        <option value="Siaga">Siaga</option>
+                                        <option value="Awas">Awas</option>
+                                    </select>
+                                </div>
+
+                                <div class="space-10"></div>
+
+                                <div class="buttonGroup">
+                                    <div class="buttonRow">
+                                        <button class="btn btn-primary" id="simpan" name="simpan">
+                                            <i class="ace-icon glyphicon glyphicon-ok bigger-120"></i>
+                                            Kirim
+                                        </button>
+                                        <button class="btn btn-danger" id="closeWindowButton">
+                                            <i class="ace-icon glyphicon glyphicon-remove bigger-120"></i>
+                                            Batal
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <!-- Proses Simpan Data -->
+                                <?php
+                                if (isset($_POST['simpan'])) {
+                                    $id_peristiwa       = "001/PRS/2020";
+                                    $korban_terdampak   = $_POST['korban_terdampak'];
+                                    $korban_mengungsi   = $_POST['korban_mengungsi'];
+                                    $korban_luka        = $_POST['korban_luka'];
+                                    $korban_meninggal   = $_POST['korban_meninggal'];
+                                    $korban_hilang      = $_POST['korban_hilang'];
+                                    $pasca_bencana      = $_POST['pasca_bencana'];
+
+                                    $LiffLaporanObservasi->update_observasi1($id_peristiwa, $korban_terdampak,
+                                        $korban_mengungsi, $korban_luka, $korban_meninggal, $korban_hilang, $pasca_bencana);
+                                    echo "<script>window.location='index.php?action=success'</script>";
+                                }
+                                ?>
+                            </form>
+                            <?php //exit; ?>
 
                             <div class="space-10"></div>
 
